434d73aaa64563eac5d12da137875969d22c0a7a
Manfred Zingl <mz@campudus.com>
2026-02-19 11:25:08 +0100

    fix: add !!!

--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,5 @@ serve({
   fetch: app.fetch,
   port: 3000
 }, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
+  console.log(`Server is running on http://localhost:${info.port}!!!`)
 })
